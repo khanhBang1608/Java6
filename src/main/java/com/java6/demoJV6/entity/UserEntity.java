0cce1eefadfca1f23530a89399e5947a91807d37
ncdao22103 <nguyenchidao117@gmail.com>
2025-04-13 23:23:30 +0700

    QuenMatKhauLogin

--- src/main/java/com/java6/demoJV6/entity/UserEntity.java
+++ src/main/java/com/java6/demoJV6/entity/UserEntity.java
@@ -28,7 +28,7 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
     @Column(name = "user_id", nullable = false)
-    private Integer id;
+    private Integer userId;
     
     @Column(name = "full_name", nullable = false, columnDefinition = "NVARCHAR(100)")
     private String name;
