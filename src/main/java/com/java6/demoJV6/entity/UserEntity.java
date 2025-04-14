491dd17a896fe35457fac9d4cb3b88aeffaaf648
ncdao22103 <nguyenchidao117@gmail.com>
2025-04-14 23:04:32 +0700

    erro

---
+++ src/main/java/com/java6/demoJV6/entity/UserEntity.java
@@ -0,0 +1,68 @@
+package com.java6.demoJV6.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @Column(name = "user_id", nullable = false)
+    private Integer userId;
+    
+    @Column(name = "full_name", nullable = false, columnDefinition = "NVARCHAR(100)")
+    private String name;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+    
+    @Column(name = "date_created")
+    private LocalDateTime dateCreated;
+    
+    @Column(name = "avatar", length = 255)
+    private String avatar;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    
+    @Column(name = "role", nullable = false)
+    private Integer role;
+
+    @Column(name = "status", nullable = false)
+    private Boolean status;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<AddressEntity> addresses;
+    
+    @Column(name = "otp", length = 255)
+    private String otp;
+
+    @Column(name = "otp_expiry")
+    private LocalDateTime otpExpiry;
+    
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private CartEntity cart;
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<FavoriteEntity> favorites;
+}
